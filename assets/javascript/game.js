d06908500c732f959fc38706a52c93606e31d89f
Hung Le <hungle913@gmail.com>
2020-02-13 13:01:59 -0800

    Set random number for game start. Console logged and noticed error for Script in HTML. Corrected link and also moved local file script tag to body for loading.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
     // Select random number to start the game. Number should be between 19-120
+    var random = Math.floor(Math.random()*101+19);
+    console.log(random);
     // Append random number to the randomNumber id on html
     // Setup random numbers for each gem. Numbers should be between 1 - 12
     // Set variable to tally wins, losses, user totals
