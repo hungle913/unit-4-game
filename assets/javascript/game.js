e784f77664999148e91751cab7dcc3a9be538df5
Hung Le <hungle913@gmail.com>
2020-02-13 13:21:52 -0800

    Edited CSS file. Added randomNumber style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var random = Math.floor(Math.random()*101+19);
     console.log(random);
     // Append random number to the randomNumber id on html
+    $("#randomNumber").text(random)
     // Setup random numbers for each gem. Numbers should be between 1 - 12
     // Set variable to tally wins, losses, user totals
     // Append Wins and Losses to wins and losses in html
