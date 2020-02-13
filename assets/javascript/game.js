da3a2b0def584e64508c0459f302107b189635c5
Hung Le <hungle913@gmail.com>
2020-02-13 12:46:43 -0800

    pseudocode laid out in js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
-    // jQuery methods go here...
+    // Select random number to start the game. Number should be between 19-120
+    // Append random number to the randomNumber id on html
+    // Setup random numbers for each gem. Numbers should be between 1 - 12
+    // Set variable to tally wins, losses, user totals
+    // Append Wins and Losses to wins and losses in html
+    // Create a reset for the game
+    // Add wins to the wins
+    // Add losses to the losses
+    // Setup click for jewels
+    // Setup wins/lose conditions
+
+
   
   });
