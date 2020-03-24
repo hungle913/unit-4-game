7f0e7800862b2e3cc5176df12615f9236863b6e3
Hung Le <hungle913@gmail.com>
2020-03-24 14:25:53 -0700

    fixed reset function. Jewels now get new values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,17 @@ $(document).ready(function(){
 
     // Select random number to start the game. Number should be between 19-120
     var random = Math.floor(Math.random()*101+19);
-    console.log(random);
+    console.log("Winning number is: " + random);
 
     // Append random number to the randomNumber id on html
-    $("#randomNumber").text("Winning Number is: " + random)
+    $("#randomNumber").text("Winning Number is: " + random);
 
     // Setup random numbers for each gem. Numbers should be between 1 - 12
     var Blue = Math.floor(Math.random()*11+1);
     var Red = Math.floor(Math.random()*11+1);
     var Yellow = Math.floor(Math.random()*11+1);
     var Green = Math.floor(Math.random()*11+1);
+    console.log(Blue, Red, Yellow, Green);
 
     // Set variable to tally wins, losses, user totals
     var wins = 0;
@@ -22,36 +23,6 @@ $(document).ready(function(){
     $("numberWins").text(wins);
     $("numberLosses").text(losses);
 
-    // Create a reset for the game
-    function reset(){
-      random = Math.floor(Math.random()*101+19);
-      console.log(random)
-      $("#randomNumber").text("Winning Number is: " + random)
-      var Blue = Math.floor(Math.random()*11+1);
-      var Red = Math.floor(Math.random()*11+1);
-      var Yellow = Math.floor(Math.random()*11+1);
-      var Green = Math.floor(Math.random()*11+1);
-      userTotal = 0;
-      $("#finalTotal").text(userTotal)
-    }
-
-
-    // Add wins to the wins
-    function win (){
-      wins ++;
-      $("#numberWins").text(wins);
-      alert ("You WON!");
-      reset();
-    }
-
-    // Add losses to the losses
-    function lose (){
-      losses++;
-      $("#numberLosses").text(losses);
-      alert ("You LOST!");
-      reset();
-    }
-
     // Setup click for jewels
     $('#Blue').on ("click", function(){
       userTotal = userTotal + Blue;
@@ -59,12 +30,18 @@ $(document).ready(function(){
       $('#finalTotal').text(userTotal); 
             //sets win/lose conditions
           if (userTotal === random){
-            win();
+            win++;
+            $("#numberWins").text(wins);
+            alert ("You WON!");
+            reset();
           }
           else if ( userTotal > random){
-            lose();
-          }   
-    })  
+            losses++;
+            $("#numberLosses").text(losses);
+            alert ("You LOST!");
+            reset();
+          };   
+    });  
 
     $("#Red").on ("click", function(){
       userTotal = userTotal + Red;
@@ -72,12 +49,18 @@ $(document).ready(function(){
       $("#finalTotal").text(userTotal); 
             //sets win/lose conditions
           if (userTotal === random){
-            win();
+            win++;
+            $("#numberWins").text(wins);
+            alert ("You WON!");
+            reset();
           }
           else if ( userTotal > random){
-            lose();
-          }   
-    })  
+            losses++;
+            $("#numberLosses").text(losses);
+            alert ("You LOST!");
+            reset();
+          };   
+    });  
 
     $("#Yellow").on ("click", function(){
       userTotal = userTotal + Yellow;
@@ -85,12 +68,18 @@ $(document).ready(function(){
       $("#finalTotal").text(userTotal); 
             //sets win/lose conditions
           if (userTotal === random){
-            win();
+            win++;
+            $("#numberWins").text(wins);
+            alert ("You WON!");
+            reset();
           }
           else if ( userTotal > random){
-            lose();
-          }   
-    })  
+            losses++;
+            $("#numberLosses").text(losses);
+            alert ("You LOST!");
+            reset();
+          };    
+    });  
 
     $('#Green').on ("click", function(){
       userTotal = userTotal + Green;
@@ -98,11 +87,31 @@ $(document).ready(function(){
       $("#finalTotal").text(userTotal); 
             //sets win/lose conditions
           if (userTotal === random){
-            win();
+            win++;
+            $("#numberWins").text(wins);
+            alert ("You WON!");
+            reset();
           }
           else if ( userTotal > random){
-            lose();
-          }   
-    })  
+            losses++;
+            $("#numberLosses").text(losses);
+            alert ("You LOST!");
+            reset();
+          };    
+    });  
+
+    // Create a reset for the game
+    function reset(){
+      random = Math.floor(Math.random()*101+19);
+      console.log("New winning number is: " + random);
+      $("#randomNumber").text("Winning Number is: " + random);
+      Blue = Math.floor(Math.random()*11+1);
+      Red = Math.floor(Math.random()*11+1);
+      Yellow = Math.floor(Math.random()*11+1);
+      Green = Math.floor(Math.random()*11+1);
+      console.log(Blue, Red, Yellow, Green);
+      userTotal = 0;
+      $("#finalTotal").text(userTotal);
+    };
   
   });
